57f8af2ace001f67c80972a2a2857f4c76939469
lahiruwanw <75386596+lahiruwanw@users.noreply.github.com>
2021-01-03 21:22:13 -0600

    commit

---
+++ _site/KNN.md
@@ -0,0 +1,22 @@
+*K*-Nearest Neighbors (K-NN)
+================
+
+*K*-Nearest Neighbors (K-NN) is a non-linear classification method. Given a positive integer *K*, the K-NN classifier identifies the *K* observations (represented the set as *N*<sub>0</sub>) nearest to an observation *x*<sub>0</sub>. It then estimates the conditional probability for category *j* as
+
+![equation](https://latex.codecogs.com/gif.latex?%5Ctext%7BPr%7D%20%28Y%3Dj%7CX%3Dx_0%29%20%3D%20%5Cfrac%7B1%7D%7BK%7D%20%5Csum_%7Bi%20%5Cin%20N_0%7D%20I%28y_i%20%3D%20j%29.)
+
+The observation *x*<sub>0</sub> assign to the category *j* with the largest conditional probability. Here are the steps of the K-NN classifier.
+
+-   Step 1: Choose the number *K* of neighbors.
+
+-   Step 2: Take the *K* nearest neighbors of the new observation *x*<sub>0</sub> using the distance measure (euclidean distance etc.)
+
+-   Step 3: Among these *K* neighbors, count the number of observations in each category.
+
+-   Step 4: Assign the new observation to the category with largest probability.
+
+[Python Code](k_nearest_neighbors.ipynb)
+
+[R Code](knn.R)
+
+The cjoice of *K* has a drastic effect on the *K*-NN classifier. When *K* = 1, the decision boundary is overly flexible but as *K* grows, the decision boundary is less flexible and that is close to linear. When *K* is large, the *K*-NN classifier has a low variance but high bias.
--- knn.R
+++ knn.R
@@ -1,4 +1,4 @@
-# Logistic Regression
+# K-Nearest Neighbors (K-NN)
 
 # Importing the dataset
 dataset = read.csv('Social_Network_Ads.csv')
@@ -19,17 +19,16 @@ test_set = subset(dataset, split == FALSE)
 training_set[-3] = scale(training_set[-3])
 test_set[-3] = scale(test_set[-3])
 
-# Fitting Logistic Regression to the Training set
-classifier = glm(formula = Purchased ~ .,
-                 family = binomial,
-                 data = training_set)
-
-# Predicting the Test set results
-prob_pred = predict(classifier, type = 'response', newdata = test_set[-3])
-y_pred = ifelse(prob_pred > 0.5, 1, 0)
+# Fitting K-NN to the Training set and Predicting the Test set results
+library(class)
+y_pred = knn(train = training_set[, -3],
+             test = test_set[, -3],
+             cl = training_set[, 3],
+             k = 5,
+             prob = TRUE)
 
 # Making the Confusion Matrix
-cm = table(test_set[, 3], y_pred > 0.5)
+cm = table(test_set[, 3], y_pred)
 
 # Visualising the Training set results
 library(ElemStatLearn)
@@ -38,10 +37,9 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+y_grid = knn(train = training_set[, -3], test = grid_set, cl = training_set[, 3], k = 5)
 plot(set[, -3],
-     main = 'Logistic Regression (Training set)',
+     main = 'K-NN (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -55,10 +53,9 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+y_grid = knn(train = training_set[, -3], test = grid_set, cl = training_set[, 3], k = 5)
 plot(set[, -3],
-     main = 'Logistic Regression (Test set)',
+     main = 'K-NN (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
